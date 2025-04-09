074f7948a88188251b0a3c3bc443ef2f32a70a3b
Fabricio A. <140019337+FbFabricio@users.noreply.github.com>
2025-04-09 13:32:41 +0000

    model para ver data do cadastro

--- oficina/models.py
+++ oficina/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.utils import timezone
+
 
 
 # Create your models here.
 class Cliente(models.Model):
+    data_atual = timezone.now()
     nome = models.CharField(max_length=100)
     carro = models.CharField(max_length=100)
     placa = models.CharField(max_length=10, unique=True)
-    '''configurar para pegar data atual do cadastro'''
+    Data_entrada = models.DateField(default=data_atual)
+    
 
     def __str__(self):
         return f"{self.nome} - {self.carro} ({self.placa})"
