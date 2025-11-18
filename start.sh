9d4899ef28178c67ad595aabf741cde0d053b491
Fabricio Andrade <contato.lorao@hotmail.com>
2025-11-18 19:57:44 -0300

    resolve bugs

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-python3 manage.py migraate --noinput
-python3 manage.py collectstatic --noinput 
-gunicorn lere.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py makemigrations --noinput 
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput 
+
+gunicorn lere.wsgi:application --bind 0.0.0.0:$PORTuPI
