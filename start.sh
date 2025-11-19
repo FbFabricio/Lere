662bb352860e10964956edca2cf818a2f13cbc44
Fabricio A. <140019337+FbFabricio@users.noreply.github.com>
2025-11-19 06:54:40 -0300

    Fix gunicorn binding port variable

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput 
 
-gunicorn lere.wsgi:application --bind 0.0.0.0:$PORTuPI
+gunicorn lere.wsgi:application --bind 0.0.0.0:$PORT
+
 
 
