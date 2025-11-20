f08c3fa951ed77d0294ff1ff4ebb46ab0b00aae4
Fabricio Andrade <contato.lorao@hotmail.com>
2025-11-20 10:14:07 -0300

    postgreSQL connect settings

--- lere/settings.py
+++ lere/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os 
+import dj_database_url
+
+
+
 
 if 'RENDER' in os.environ:
     DEBUG = False
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,13 +101,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
+
 python manage.py makemigrations --noinput 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput 
 
-gunicorn lere.wsgi:application --bind 0.0.0.0:$PORTuPI
-=======
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput 
 gunicorn lere.wsgi:application --bind 0.0.0.0:$PORT
->>>>>>> 7b8dcbb67fb106159a28ec7d47538139ed678784
+
+
